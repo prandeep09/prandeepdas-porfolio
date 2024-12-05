4a4d17afb10b057372173bae452a2563892dcae0
prandeep09 <prandp.ol09@outlook.com>
2024-12-05 13:36:34 +0530

    case study image bug fix 2

--- src/metadata/casestudyimages.js
+++ src/metadata/casestudyimages.js
@@ -13,6 +13,16 @@ import image12CS1src from "../assets/CS-CSS--S12-min.jpg";
 import image13CS1src from "../assets/CS-CSS--S13-min.jpg";
 import image14CS1src from "../assets/CS-CSS--S14-min.jpg";
 import image15CS1src from "../assets/CS-CSS--S15-min.jpg";
+import image1CS2src from "../assets/CS2_S01-min.jpg";
+import image2CS2src from "../assets/CS2_S02-min.jpg";
+import image3CS2src from "../assets/CS2_S03-min.jpg";
+import image4CS2src from "../assets/CS2_S04-min.jpg";
+import image5CS2src from "../assets/CS2_S05-min.jpg";
+import image6CS2src from "../assets/CS2_S06-min.jpg";
+import image7CS2src from "../assets/CS2_S07-min.jpg";
+import image8CS2src from "../assets/CS2_S08-min.jpg";
+import image9CS2src from "../assets/CS2_S09-min.jpg";
+import image10CS2src from "../assets/CS2_S10-min.jpg";
 const casestudyImages = [
   {
     id: "CS1",
@@ -41,52 +51,52 @@ const casestudyImages = [
       {
         id: 5,
         src: image5CS1src,
-        alt: "case study goal and challenges",
+        alt: "research and analysis of the existing UI",
       },
       {
         id: 6,
         src: image6CS1src,
-        alt: "homepage redesign process",
+        alt: "case study goal",
       },
       {
         id: 7,
         src: image7CS1src,
-        alt: "product page redesign process",
+        alt: "case study challenges",
       },
       {
         id: 8,
         src: image8CS1src,
-        alt: "search feature redesign process",
+        alt: "fianl solution UI and NDA disclaimer",
       },
       {
         id: 9,
         src: image9CS1src,
-        alt: "dashboard redesign process",
+        alt: "validation technique and the journey",
       },
       {
         id: 10,
         src: image10CS1src,
-        alt: "takeaways and contact details",
+        alt: "initial usability findings",
       },
       {
         id: 11,
         src: image11CS1src,
-        alt: "takeaways and contact details",
+        alt: "second iteration detials",
       },
       {
         id: 12,
         src: image12CS1src,
-        alt: "takeaways and contact details",
+        alt: "final challange and solution",
       },
       {
         id: 13,
         src: image13CS1src,
-        alt: "takeaways and contact details",
+        alt: "final matrics",
       },
       {
         id: 14,
         src: image14CS1src,
-        alt: "takeaways and contact details",
+        alt: "before and after user flows comparison",
       },
       {
         id: 15,
@@ -101,52 +111,52 @@ const casestudyImages = [
     imagedata: [
       {
         id: 1,
-        src: "src/assets/CS2_S01-min.jpg",
+        src: image1CS2src,
         alt: "case study title and hero image",
       },
       {
         id: 2,
-        src: "src/assets/CS2_S02-min.jpg",
+        src: image2CS2src,
         alt: "context and problem statement",
       },
       {
         id: 3,
-        src: "src/assets/CS2_S03-min.jpg",
+        src: image3CS2src,
         alt: "my role in the project; product designer and developer",
       },
       {
         id: 4,
-        src: "src/assets/CS2_S04-min.jpg",
+        src: image4CS2src,
         alt: "case study overview explaining solutions and process",
       },
       {
         id: 5,
-        src: "src/assets/CS2_S05-min.jpg",
+        src: image5CS2src,
         alt: "case study goal and challenges",
       },
       {
         id: 6,
-        src: "src/assets/CS2_S06-min.jpg",
+        src: image6CS2src,
         alt: "homepage redesign process",
       },
       {
         id: 7,
-        src: "src/assets/CS2_S07-min.jpg",
+        src: image7CS2src,
         alt: "product page redesign process",
       },
       {
         id: 8,
-        src: "src/assets/CS2_S08-min.jpg",
+        src: image8CS2src,
         alt: "search feature redesign process",
       },
       {
         id: 9,
-        src: "src/assets/CS2_S09-min.jpg",
+        src: image9CS2src,
         alt: "dashboard redesign process",
       },
       {
         id: 10,
-        src: "src/assets/CS2_S10-min.jpg",
+        src: image10CS2src,
         alt: "takeaways and contact details",
       },
     ],
